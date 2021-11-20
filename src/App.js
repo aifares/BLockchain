a6fc595e38557b53cf897d3b8384663a036599bb
Ali Fares <aifares@buffalo.edu>
2021-11-20 14:46:37 -0500

    Added everything #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import WalletCard from "./walletConfig";
+import TextField from "@mui/material/TextField";
+import Web3 from "web3";
+
 
 function App() {
+ 
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>RESTURANT RESERVATION SYSTEM</div>
+      <div>
+        {" "}
+        <Button variant="contained">Contained</Button>
+      </div>
+      <WalletCard/>
+
+      
     </div>
   );
 }
---
+++ src/walletConfig.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import ABIFILE from "./ABIFILE.json";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+const SimpleStorage = () => {
+  // deploy simple storage contract and paste deployed contract address here. This value is local ganache chain
+  let contractAddress = "0x2811d139974AB83f53125aAdC395b6102689792b";
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const [userBalance, setUserBalance] = useState(null);
+  const [billTotal, setBIllTotal] = useState(null);
+
+  const [currentContractVal, setCurrentContractVal] = useState(null);
+
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+          setConnButtonText("Wallet Connected");
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log("Need to install MetaMask");
+      setErrorMessage("Please install MetaMask browser extension to interact");
+    }
+  };
+
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    updateEthers();
+  };
+
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  };
+
+  // listen for account changes
+  window.ethereum.on("accountsChanged", accountChangedHandler);
+
+  window.ethereum.on("chainChanged", chainChangedHandler);
+
+  const updateEthers = () => {
+    let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(tempProvider);
+
+    let tempSigner = tempProvider.getSigner();
+    setSigner(tempSigner);
+
+    let tempContract = new ethers.Contract(
+      contractAddress,
+      ABIFILE,
+      tempSigner
+    );
+    setContract(tempContract);
+  };
+
+  const setReservation = (event) => {
+    event.preventDefault();
+    contract.reserve(
+      event.target.setPartySize.value,
+      event.target.setTime.value
+    );
+  };
+
+  const setCheckIn = (event) => {
+    event.preventDefault();
+    contract.Checkin();
+  };
+
+
+  const pay = (event) => {
+    event.preventDefault();
+    contract.paid("0xe0119d0B935236593b54055b2c0F49D4C67215b5");
+  };
+
+  const addToBill = (event) => {
+    event.preventDefault();
+    contract.addToBill(event.target.amount.value);
+  };
+
+  const seeBill = async () => {
+      console.log("SeeBill")
+    let val = await contract.SeeMyBill();
+    let decimal = parseInt(val, 16);
+    setBIllTotal(decimal);
+  };
+
+  return (
+    <div>
+      <h4> {"Get/Set Contract interaction"} </h4>
+      <button onClick={connectWalletHandler}>{connButtonText}</button>
+      <div>
+        <h3>Address: {defaultAccount}</h3>
+      </div>
+      <form onSubmit={setReservation}>
+        <TextField
+          variant="outlined"
+          id="setPartySize"
+          type="text"
+          placeholder="Party Size"
+        />
+        <TextField
+          variant="outlined"
+          type="text"
+          placeholder="Reservavtion Time"
+        />
+        <Button variant="contained" type={"submit"}>
+          {" "}
+          Make Reservavtion{" "}
+        </Button>
+      </form>
+      <form onSubmit={addToBill}>
+        <TextField
+          variant="outlined"
+          id="amount"
+          type="text"
+          placeholder="Amount To Add"
+        />
+        <div>
+          <Button variant="contained" type={"submit"}>
+            {" "}
+            Add To Bill{" "}
+          </Button>
+        </div>
+      </form>
+      
+      <div>
+        <Button onClick={setCheckIn} variant="contained">
+          Check In
+        </Button>
+      </div>
+
+      <div>
+        <Button onClick={seeBill} variant="contained">
+          See My Bill
+        </Button>
+      </div>
+      {errorMessage}
+      {billTotal}
+    </div>
+  );
+};
+
+export default SimpleStorage;
